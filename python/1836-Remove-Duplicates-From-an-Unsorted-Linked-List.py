7d389d2f3045b23faf6aa1484221c8c12affc787
guidiamond <guilherme.batista770@gmail.com>
2022-12-04 19:14:42 -0300

    Add 1836 solution in python

---
+++ python/1836-Remove-Duplicates-From-an-Unsorted-Linked-List.py
@@ -0,0 +1,25 @@
+import collections
+
+
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution(object):
+    def deleteDuplicatesUnsorted(self, head):
+        count = collections.defaultdict(int)
+        curr = head
+
+        while curr:
+            count[curr.val] += 1
+            curr = curr.next
+
+        curr = dummy = ListNode(0, head)
+
+        while curr.next:
+            if count[curr.next.val] == 1:
+                curr = curr.next
+            else:
+                curr.next = curr.next.next
+        return dummy.next
